8860c0b778e475041063085438e6295a2f87a557
d0dders <dodders@outlook.com>
2019-04-10 20:54:59 +0100

    Wire ToText to the menu and tidy up menu

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -12,16 +12,59 @@ namespace MorseCodeTranslator
         {
             while (true)
             {
-                Console.Write("Enter text to translate: ");
-                string input = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(input))
+                Console.WriteLine("Select what you want to translate:");
+                Console.WriteLine("1. Text to Morse");
+                Console.WriteLine("2. Morse to Text");
+                Console.WriteLine("Q. Quit");
+                string selection = Console.ReadLine();
+                switch (selection.ToUpper())
                 {
-                    break;
+                    case "1":
+                        TextToMorse();
+                        break;
+                    case "2":
+                        MorseToText();
+                        break;
+                    case "Q":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("That was not a vlid option, try again:");
+                        break;
                 }
-                string output = MorseCodeTranslator.ToMorse(input);
 
-                Console.WriteLine(output);
             }
+        }
+
+        private static void MorseToText()
+        {
+            Console.Clear();
+            Console.Write("Enter text to translate: ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+            string output = MorseCodeTranslator.ToText(input);
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+        }
+
+        static void TextToMorse()
+        {
+            Console.Clear();
+            Console.Write("Enter text to translate: ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+            string output = MorseCodeTranslator.ToMorse(input);
+
+            Console.WriteLine(output);
+            Console.WriteLine();
         }
     }
 }
